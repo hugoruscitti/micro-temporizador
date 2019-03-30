14a8b4e5f2652f176c4b6a47c3362e345bee233a
Hugo Ruscitti <hugoruscitti@gmail.com>
2019-03-30 11:22:13 -0300

    Agregando trayicon.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain, Tray } = require("electron");
+const path = require("path");
 
 let mainWindow;
+let tray = undefined;
+const recursos = path.join(__dirname, "recursos");
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -22,7 +25,41 @@ function createWindow() {
   });
 }
 
-app.on("ready", createWindow);
+const createTray = () => {
+  tray = new Tray(path.join(recursos, "icono.png"));
+  tray.on("right-click", toggleWindow);
+  tray.on("double-click", toggleWindow);
+
+  tray.on("click", function(event) {
+    toggleWindow();
+
+    /*
+    // Show devtools when command clicked
+    if (window.isVisible() && process.defaultApp && event.metaKey) {
+      window.openDevTools({mode: 'detach'})
+    }
+    */
+  });
+};
+
+const toggleWindow = () => {
+  if (mainWindow.isVisible()) {
+    mainWindow.hide();
+  } else {
+    showWindow();
+  }
+};
+
+const showWindow = () => {
+  mainWindow.show();
+  mainWindow.focus();
+};
+
+app.dock.hide();
+app.on("ready", () => {
+  createWindow();
+  createTray();
+});
 
 app.on("window-all-closed", function() {
   if (process.platform !== "darwin") {
